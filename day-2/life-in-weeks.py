1c9ff3f32cfe1dec3b02e2d16bb222e6da28b8c6
Ezekiel <iamwebwiz@gmail.com>
2021-11-23 16:45:04 +0100

    add life-in-weeks file

---
+++ day-2/life-in-weeks.py
@@ -0,0 +1,13 @@
+age = input("What is your current age?")
+
+TOTAL_YEARS = 90
+
+age_as_number = int(age)
+years_remaining = TOTAL_YEARS - age_as_number
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+output = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+
+print(output)
