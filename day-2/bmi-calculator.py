968cb8799a665cfa2a47219c0e49ac41196b82f4
Ezekiel <iamwebwiz@gmail.com>
2021-11-23 15:15:14 +0100

    refactor code

--- day-2/bmi-calculator.py
+++ day-2/bmi-calculator.py
@@ -1,6 +1,6 @@
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 
-body_mass_index = int(float(weight) / (float(height) * float(height)))
+body_mass_index = int(int(weight) / (float(height) ** 2))
 
 print(body_mass_index)
