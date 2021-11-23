13323756f9d7a48031cfed0cd8e6ce0649e8a4bf
Ezekiel <iamwebwiz@gmail.com>
2021-11-23 15:12:06 +0100

    update bmi-calculator

---
+++ day-2/bmi-calculator.py
@@ -0,0 +1,6 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+body_mass_index = int(float(weight) / (float(height) * float(height)))
+
+print(body_mass_index)
