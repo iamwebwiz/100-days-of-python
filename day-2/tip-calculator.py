fa1bace5d792f92647e333c7a98faf1346a7a716
Ezekiel <iamwebwiz@gmail.com>
2021-11-24 15:44:22 +0100

    refactor tip calculator

--- day-2/tip-calculator.py
+++ day-2/tip-calculator.py
@@ -2,14 +2,14 @@ print("Welcome to the tip calculator!")
 print("------------------------------")
 bill = input("What was the total bill? $")
 tip_percentage = input("What percentage tip would you like to give? 10, 12 or 15? ")
-splitters = input("How many people to split the bill? ")
+people = input("How many people to split the bill? ")
 
 bill_as_number = float(bill)
 tip_percentage_as_int = int(tip_percentage)
 tip = bill_as_number * (tip_percentage_as_int / 100)
 total_bill = bill_as_number + tip
-splits = total_bill / int(splitters)
+bill_per_person = total_bill / int(people)
 
-message = f"Each person should pay: ${round(splits, 2)}"
+message = f"Each person should pay: ${bill_per_person:.2f}"
 
 print(message)
