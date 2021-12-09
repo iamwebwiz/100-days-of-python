9002e545fee7646c50e7feb9901b8d53ee9ba7c8
Ezekiel <iamwebwiz@gmail.com>
2021-12-09 15:56:18 +0100

    refactor

--- day-3/pizza-order.py
+++ day-3/pizza-order.py
@@ -7,15 +7,15 @@ bill = 0
 
 if size == "S":
     bill = 15
-    if add_pepperoni == "Y":
-        bill += 2
 elif size == "M":
     bill = 20
-    if add_pepperoni == "Y":
-        bill += 3
 else:
     bill = 25
-    if add_pepperoni == "Y":
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
         bill += 3
 
 if extra_cheese == "Y":
